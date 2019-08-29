9acf5ed732cc520742571cc865996cd7cebe2ba4
MikeZhangpy <zhangpy19911121@gmail.com>
2019-08-29 10:01:35 +0800

    Initial Commit

---
+++ PYModel/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PYModel
+//
+//  Created by 张鹏宇 on 2019/8/29.
+//  Copyright © 2019 Wanye1121. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
